24fcd13b7f244a1e33881b25de322578a18f572a
MolyMade <ifdog@outlook.com>
2022-07-20 14:48:54 +0800

    Update armv8.mk

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -71,6 +71,16 @@ define Device/friendlyarm_nanopi-r4se
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r4se
 
+define Device/friendlyarm_nanopi-r5s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R5S
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := nanopi-r4s-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-bin | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8125 -urngd
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r5s
+
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
